921d7d00bba33d4c8802d5c25ceb2d268bede999
bruunooAlmeida <almeidabrunoti@gmail.com>
2023-08-05 13:29:09 -0300

    Iniciando Projeto

---
+++ twoDay.js
@@ -0,0 +1,24 @@
+/*
+Variáveis são os blocos básicos de construção de qualquer sistema e são essenciais para processar qualquer tipo de informação, seja ela de uma pessoa logada no sistema ou mesmo para exibir detalhes de produtos em um catálogo de e-commerce.
+
+Por isso, hoje, eu vou te ensinar a desenvolver um programa simulando um desses sites. Ele deve pedir para o usuário responder 3 perguntas:
+
+- Qual o seu nome?
+- Quantos anos você tem?
+- Qual linguagem de programação você está estudando?
+
+À medida que as perguntas forem sendo feitas, a pessoa que estiver usando o programa deve responder cada uma delas.
+
+No final, o sistema vai exibir a mensagem:
+
+"Olá [nome], você tem [idade] anos e já está aprendendo [linguagem]!"
+
+Note que cada informação entre [ ] é uma das respostas dadas pela pessoa.
+*/
+
+
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos ans voce tem?");
+const linguagem = prompt("Qual a linguagem voce esta estudando?");
+
+console.log(`Ola ${nome}, voce tem ${idade} anos e ja esta aprendendo ${linguagem}!`);
